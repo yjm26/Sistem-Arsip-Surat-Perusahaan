c7cd396fab2981ff6e387edadff16b8cba3765bc
yjm26 <rivaldojntr@gmail.com>
2025-04-22 17:50:51 +0700

    feat (dashboard) : some update

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Login from "@/components/pages/login";
-import Dashboard from "@/components/pages/dashboard";
+import Login from "@/pages/login";
+import Dashboard from "@/pages/dashboard";
 
 
 function App() {
--- src/components/ui/app-statistic-card.jsx
+++ src/components/ui/app-statistic-card.jsx
@@ -1,5 +1,5 @@
 import statisticsCardsData from "@/data/statistics-cards-data";
-import { Card, CardContent } from "@/components/ui/card";
+import { Card, CardContent, CardFooter } from "@/components/ui/card";
 
 export function StatisticsCards() {
   return (
@@ -7,16 +7,25 @@ export function StatisticsCards() {
       {statisticsCardsData.map((stat, index) => (
         <Card
           key={index}
-          className="p-4 bg-white  rounded-lg w-[335px] h-[235px] justify-center" // 50% width minus gap
+          className="flex bg-white rounded-lg w-[400px] h-[150px] items-center justify-between"
         >
-          <CardContent className="flex flex-col items-center justify-center">
+          <CardContent className="flex items-center justify-evenly ">
             {/* Icon */}
-            <stat.icon className={`w-10 h-10 text-${stat.color}-500 mb-2`} />
+            <div className="">
+            <stat.icon className={`w-[120px]  h-[80px] stroke-[0.5px]`} />
+            </div>
+
+            <div className="">
             {/* Title */}
             <h2 className="text-sm font-medium text-gray-600">{stat.title}</h2>
             {/* Value */}
-            <p className="text-3xl font-bold text-gray-900">{stat.value}</p>
+            <p className="text-3xl font-semibold text-gray-900">{stat.value}</p>
+            </div>
           </CardContent>
+
+          <CardFooter>
+
+          </CardFooter>
         </Card>
       ))}
     </div>
